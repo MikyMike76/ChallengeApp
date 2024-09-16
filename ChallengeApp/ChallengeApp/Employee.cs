f4deade061d7612e8da792b7aa658d730e620ec2
MikyMike76 <michalgolab01@gmail.com>
2024-09-16 12:08:44 +0200

    Zadanie domowe dzień 6.

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Score { get; private set; }
+
+        public Employee (string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Score = 0;
+        }
+
+        public void AddScore (int score)
+        {
+            this.Score += score;
+        }
+
+        
+
+
+        
+
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,40 @@
-﻿int number = 4566;
-string numberToString = number.ToString();
-char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-int[] repeats = new int[10];
-foreach (char c in numberToString)
+﻿using ChallengeApp;
+
+Employee employee1 = new Employee("Michal", "Kowalski", 55);
+Employee employee2 = new Employee("Aneta", "Borkowska", 34);
+Employee employee3 = new Employee("Artur", "Zagrabski", 23);
+
+employee1.AddScore(5);
+employee2.AddScore(2);
+employee3.AddScore(1);
+employee1.AddScore(3);
+employee2.AddScore(4);
+employee3.AddScore(8);
+employee1.AddScore(7);
+employee2.AddScore(3);
+employee3.AddScore(0);
+employee1.AddScore(10);
+employee2.AddScore(9);
+employee3.AddScore(10);
+employee1.AddScore(1);
+employee2.AddScore(7);
+employee3.AddScore(8);
+
+List<Employee> employees = new List<Employee>()
 {
-    for (int i = 0; i < digits.Length; i++)
+    employee1, employee2, employee3
+};
+
+int maxScore = 0;
+Employee MaxScoreEmp = null;
+
+foreach (var employee in  employees)
+{
+    if (employee.Score > maxScore)
     {
-        if(c == digits[i])
-        {
-            repeats[i]++;
-        }
+        maxScore = employee.Score;
+        MaxScoreEmp = employee;
     }
 }
 
-for (int i = 0; i < digits.Length; i ++)
-{
-    Console.WriteLine("Cyfra {0} wystąpiła {1} razy", digits[i], repeats[i]);
-}
-
+Console.WriteLine("Imię, nazwisko i punktacja najlepszego: {0} {1} - {2} pkt", MaxScoreEmp.Name, MaxScoreEmp.Surname, maxScore);
