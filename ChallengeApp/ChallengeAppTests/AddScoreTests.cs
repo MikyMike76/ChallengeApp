da539ee8cd798fa5232e2149b5a14b9406eb11ea
Michał Golub <michalgolab01@gmail.com>
2024-09-17 11:24:48 +0200

    Zadanie domowe dzień 7.

---
+++ ChallengeApp/ChallengeAppTests/AddScoreTests.cs
@@ -0,0 +1,22 @@
+namespace ChallengeAppTests
+{
+    public class Tests
+    {
+        
+        [Test]
+        public void WhenAddScore_ShouldCorrectResult()
+        {
+            //Arrange
+            var employee = new Employee("Michal", "Golub", 22);
+            employee.AddScore(3);
+            employee.AddScore(5);
+            employee.AddScore(-11);
+
+            //Act
+            var result = employee.Score;
+
+            //Assert
+            Assert.AreEqual(-3, result);
+        }
+    }
+}
