b14e1259bee7045ab2bcb1d41f065fce340128f5
Michał Golub <michalgolab01@gmail.com>
2024-09-23 14:08:52 +0200

    Zadanie domowe dzień 14.

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -4,17 +4,19 @@ using System.Diagnostics;
 
 namespace ChallengeApp
 {
-    public class Employee
-    {
-        public string Name { get; private set; }
-        public string Surname { get; private set; }
-        
+    public class Employee : Person
+    {  
         private List<float> grades = new List<float>();
 
         public Employee(string name, string surname)
+            : base(name, surname)
         {
-            this.Name = name;
-            this.Surname = surname;
+
+        }
+        public Employee(string name, string surname, char sex) 
+            :base (name, surname, sex)
+        {
+            
         }
 
         public void AddGrade (float grade)
